949ea688e3633e35f5eee7bd9c6dc65642f19dd3
curiotim <curiotim@curioadmins-MacBook-Pro.local>
2016-12-29 14:01:15 +0800

    Changed to Express

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps! Push Test</h1>');
-}).listen(process.env.PORT);
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
